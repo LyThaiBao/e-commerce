2948dc13c32c3e4dcf69b77cf96b11eceb329c09
Lý Thái Bảo <baoly069@gmail.com>
2025-08-23 09:49:12 +0700

    add effect skeleton

--- js/detail/modules/renderProduct.js
+++ js/detail/modules/renderProduct.js
@@ -27,6 +27,9 @@ export function renderProductDetail(data) {
   let productPrice = document.querySelector(".product__price span");
   let productColor = document.querySelectorAll(".product__color span");
   let productSize = document.querySelector(".product__size");
+  let containDetail = document.querySelector(".main__detail--product");
+  console.log(containDetail);
+
   // --------------------Render--------------------------------------
   // --------------------Image of gallery---------------------------------------
   gallery.innerHTML = `
@@ -37,25 +40,46 @@ export function renderProductDetail(data) {
                 <img src="${currentProduct.image[1]}" alt="" />
      </div>
   `;
-  // ---------------------Name_Price_Color of product-------------------------------------
-  //NAME
-  productName.innerHTML = currentProduct.name;
-  //PRICE
-  productPrice.innerHTML = formatVND(currentProduct.price);
-  //COLOR
-  productColor[0].style.backgroundColor =
-    currentProduct.color[0] != undefined ? currentProduct.color[0] : "orange";
-  productColor[1].style.backgroundColor =
-    currentProduct.color[1] != undefined ? currentProduct.color[1] : "orange";
-  // --------------------------Render box Size--------------------------------------------
-  productSize.innerHTML = `
- <strong>Size:</strong>
-  <span>${currentProduct?.size[0]}</span>
-  <span>${currentProduct?.size[1]}</span>
-  <span>${currentProduct?.size[2]}</span>
-  <span>${currentProduct?.size[3]}</span>
+  // ---------------------Render Text-------------------------------------
+  containDetail.innerHTML = `
+ <h1 class="product__name--detail">${currentProduct.name}</h1>
+            <div class="product__price">
+              <strong>Giá:</strong>
+              <span class="">${formatVND(currentProduct.price)}</span>
+            </div>
+            <div class="product__color">
+              <strong>Màu:</strong>
+              <span style = "background-color:${
+                currentProduct.color[0]
+              }" class=""></span>
+              <span  style ="background-color:${
+                currentProduct.color[1]
+              }" class=""></span>
+            </div>
+            <div class="product__size">
+              <strong>Size:</strong>
+              <span class="">${currentProduct.size[0]}</span>
+              <span class="">${currentProduct.size[1]}</span>
+              <span class="">${currentProduct.size[2]}</span>
+              <span class="">${currentProduct.size[3]}</span>
+            </div>
+            <div class="product__quantity">
+              <strong>Số Lượng</strong>
+              <button class="reduce">-</button>
+              <input
+                
+                type="number"
+                value="1"
+                name="quality"
+                min="1"
+              />
+              <button class="increase">+</button>
+            </div>
+            <div class="product__cta">
+              <div class="addCart" style="background-color: #eee">Thêm vào giỏ hàng</div>
+              <div class="buyNow">Mua Ngay</div>
+            </div>
 `;
-
   // ------------------------Render Description product--------------------------------
   let detailProduct = document.querySelector(".main__detail");
   detailProduct.innerHTML = `
